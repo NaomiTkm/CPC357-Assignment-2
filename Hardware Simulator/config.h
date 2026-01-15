e1a0992e5e2bec93541851e5ef745c0318e342b3
NaomiTkm <121533158+NaomiTkm@users.noreply.github.com>
2026-01-16 05:44:51 +0800

    Update Wi-Fi credentials in config.h

--- Hardware Simulator/config.h
+++ Hardware Simulator/config.h
@@ -3,12 +3,13 @@
 // ==========================================
 
 // Wi-Fi Credentials
-const char* ssid = "cslab";
-const char* password = "aksesg31";
+const char* ssid = "wifi ssid";
+const char* password = "wifi password";
 
 // Google Cloud VM Details
 // This is the External Static IP of your VM running Mosquitto
 const char* mqtt_server = "35.209.78.195"; 
 const int mqtt_port = 1883;
 const char* mqtt_topic = "inverter/readings";
+
 const char* device_id = "inverter_001";
